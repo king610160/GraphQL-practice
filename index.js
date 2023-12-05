bc030ceeb5e33dec109c5605fa830accc3cf03bf
king610160 <king610160@gmail.com>
2023-12-05 10:53:02 +0800

    basic setup for graphQL and apollo server.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import { ApolloServer } from '@apollo/server'
+import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from '@apollo/server/standalone'
+let PORT = 4000
 
 // server setup
 const server = new ApolloServer({
-  // typeDefs
-  // resolvers
+    // typeDefs
+    // resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 }
+    listen: { port: PORT }
 })
 
-console.log(`Server ready at: ${url}`)
+console.log(`Server is ready on Port ${PORT}`)
